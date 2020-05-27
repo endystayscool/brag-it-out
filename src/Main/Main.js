d28799394c9f6e955934fd0d95d15dc51d51781a
endystayscool <enendy@gmail.com>
2020-05-27 15:08:36 +0200

    dark noght tx

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -7,7 +7,7 @@ function Main() {
     useEffect(() => {
         // initial
         var scene = new THREE.Scene();
-        scene.background = new THREE.Color(0x000000);
+        scene.background = new THREE.Color(0x191919);
         var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
         camera.position.z = 1.5;
         var renderer = new THREE.WebGLRenderer();
@@ -32,14 +32,14 @@ function Main() {
         var loader = new THREE.TextureLoader();
 
         const material = new THREE.MeshBasicMaterial({
-            map: loader.load('https://i.imgur.com/zoqIFOp.jpg')
+            map: loader.load('https://i.imgur.com/45naBE9.jpg')
         });
         var earthmesh = new THREE.Mesh(geometry, material);
         globe.add(earthmesh);
 
         globe.position.y = .25;
         globe.rotation.x = .3;
-        camera.position.z = 2;
+        camera.position.z = 2.3;
 
         function animate() {
             renderer.render(scene, camera);
